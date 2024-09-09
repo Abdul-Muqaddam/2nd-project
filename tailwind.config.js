74b0d1d4eb5007a047ac5d2f13f0ef4a1b6ccf33
Abdul-Muqaddam <chandansar444999@gmail.com>
2024-09-09 22:07:28 +0500

    Responsiveness of the (A-Room) project is completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./Actual_code/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        "mq-950":{max:"950px"},
+        "mq-750":{max:"750px"},
+        "mq-1050":{max:"1050px"},
+        "mq-500":{max:"500px"},
+        "mq-600":{max:"600px"},
+        "mq-1130":{max:"1130px"},
+        "mq-1060":{max:"1060px"},
+        "mq-360":{max:"360px"}
+      }
+    },
   },
   plugins: [],
 }
